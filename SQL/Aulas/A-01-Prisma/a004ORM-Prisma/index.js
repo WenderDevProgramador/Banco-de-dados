fc33fda69c7ee996099e6b885ca46ada1bb52950
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2026-02-08 19:36:10 -0300

    Aula init

---
+++ SQL/Aulas/A-01-Prisma/a004ORM-Prisma/index.js
@@ -0,0 +1,27 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+async function main() {
+    await prisma.post.createMany({
+        data: [
+            {
+                title: 'Olá, mundo!',
+                content: 'Este é meu primeiro post usando Prisma.',
+                published: true
+            },
+
+            {
+                title: 'Prisma é incrível!',
+                content: 'Estou adorando usar Prisma para gerenciar meu banco de dados.',
+                published: true
+            }
+        ]
+    })
+}
+
+main().then(async () => {
+    await prisma.$disconnect()
+})
+
+
+
