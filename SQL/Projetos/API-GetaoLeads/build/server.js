184f2a0020e85b5397ba605584a9d59046c1d0f8
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2026-03-01 18:33:47 -0300

    Exluido para tentar novamente para correção de erro

--- SQL/Projetos/API-GetaoLeads/build/server.js
+++
@@ -1,9 +0,0 @@
-import cors from 'cors';
-import express from 'express';
-const app = express();
-app.use(cors());
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-//# sourceMappingURL=server.js.map
