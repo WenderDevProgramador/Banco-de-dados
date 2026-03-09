c1ef6ce7fc353ecebba077acfd9441219c35b5c1
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2026-03-09 18:43:04 -0300

    rotas e metodos put e delete add

--- SQL/Projetos/API-Leads/src/controllers/LeadsController.ts
+++ SQL/Projetos/API-Leads/src/controllers/LeadsController.ts
@@ -1,6 +1,6 @@
 import type { Handler } from "express";
 import { prisma } from "../database/index.js";
-import { CreateLeadRequestSchema } from "./schemas/LeadsRequestSchema.js";
+import { CreateLeadRequestSchema, UpdateLeadRequestSchema } from "./schemas/LeadsRequestSchema.js";
 import { HttpError } from "../errors/HttpError.js";
 
 export class LeadsController {
@@ -46,7 +46,44 @@ export class LeadsController {
             }
 
             res.json(lead)
-            
+
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    update: Handler = async (req, res, next) => {
+        try {
+            const body = UpdateLeadRequestSchema.parse(req.body)
+
+            const leadExists = await prisma.lead.findUnique({
+                where: { id: Number(req.params.id) }
+            })
+
+            if (!leadExists) {
+                throw new HttpError(404, "Lead not found")
+            }
+
+            const updatedLead = await prisma.lead.update({
+                where: { id: Number(req.params.id) },
+                data: body
+            })
+            res.json(updatedLead)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    delete: Handler = async (req, res, next) => {
+        try {
+            const id = Number(req.params.id)
+
+            const leadExists = await prisma.lead.findUnique({ where: { id } })
+            if (!leadExists) throw new HttpError(404, "lead não encontrado")
+
+            const deletedLead = await prisma.lead.delete({ where: { id } })
+
+            res.json({ deletedLead })
         } catch (error) {
             next(error)
         }
