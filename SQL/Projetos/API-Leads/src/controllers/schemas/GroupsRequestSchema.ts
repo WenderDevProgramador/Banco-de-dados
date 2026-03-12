84e15dc3a48ff0edd8cc15216c30065f61c8a18f
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2026-03-12 18:47:48 -0300

    Rotas de Grup criadas

---
+++ SQL/Projetos/API-Leads/src/controllers/schemas/GroupsRequestSchema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+export const CreateGroupRequestSchema = z.object({
+    name: z.string(),
+    description: z.string()
+})
+
+export const UpdateGroupRequestSchema = z.object({
+    name: z.string().optional(),
+    description: z.string().optional()
+})
